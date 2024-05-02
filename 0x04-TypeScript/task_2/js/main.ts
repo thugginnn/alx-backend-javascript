c4446ca43f4b63c5b9088de4ff6948bb84d0b8e2
thugginnn <paul.iyiade4@gmail.com>
2024-05-02 20:22:40 +0000

    tmain

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,47 +1,38 @@
 interface DirectorInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workDirectorTasks(): string;
-  }
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
   
-  interface TeacherInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workTeacherTasks(): string;
-  }
-  
-  export class Director implements DirectorInterface {
-    workFromHome = () => 'Working from home';
-    getCoffeeBreak = () => 'Getting a coffee break';
-    workDirectorTasks = () => 'Getting to director tasks';
-  }
-  
-  export class Teacher implements TeacherInterface {
-    workFromHome = () => 'Cannot work from home';
-    getCoffeeBreak = () => 'Cannot have a break';
-    workTeacherTasks = () => 'Getting to work';
-  }
-  
-  export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director()
-  
-  export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
-    return (employee as Director).workDirectorTasks !== undefined;
-  }
-  
-  export function executeWork(employee: DirectorInterface | TeacherInterface): string {
-    let res = undefined;
-    (isDirector(employee)) ? res = employee.workDirectorTasks() : res = employee.workTeacherTasks();
-    return res;
-  }
-  type Subjects = "Math" | "History";
-  
-  export function teachClass(todayClass: Subjects): string {
-    if (todayClass === "Math") {
-      return "Teaching Math";
-    } else if (todayClass === "History") {
-      return "Teaching History";
-    }
-  }
-  
-  console.log(teachClass("Math"));
-  console.log(teachClass("History"));
+class Director implements DirectorInterface {
+  workFromHome = () : string => 'Working from home';
+  getCoffeeBreak = () : string => 'Getting a coffee break';
+  workDirectorTasks = () : string => 'Getting to director tasks';
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome = () : string => 'Cannot work from home';
+  getCoffeeBreak = () : string => 'Cannot have a break';
+  workTeacherTasks = () : string => 'Getting to work';
+}
+
+export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
+
+export const isDirector = (employee: Teacher | Director): boolean => employee instanceof Director;
+
+export const executeWork = (employee: Teacher | Director): void => {
+  const work = employee instanceof Teacher? employee.workTeacherTasks() : employee.workDirectorTasks();
+  console.log(work)
+}
+
+type Subjects = 'Math' | 'History';
+
+export const teachClass = (todayClass: Subjects) : string => {
+  return todayClass === 'Math' ? 'Teaching Math': 'Teaching History';
+}
