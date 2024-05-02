1a0538c2884cf42e0658ca7f4190d92d7a517f0e
thugginnn <paul.iyiade4@gmail.com>
2024-05-02 20:46:56 +0000

    java

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,15 @@
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class JavaClass extends Subject{
+    getRequirements = () => 'Here is the list of requirements for Java';
+
+    getAvailableTeacher = () => {
+      if (!this.teacher?.experienceTeachingJava) return 'No available teacher';
+
+      return `Available Teacher: ${this.teacher}`;
+    } 
+  }
+}
